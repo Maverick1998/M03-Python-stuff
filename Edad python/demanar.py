af2917f436c0cfd930826a7bb6dc599b86fd671e
Maverick1998 <rlrebazamor.elluch@gmail.com>
2017-03-17 12:30:34 +0100

    Add files via upload

---
+++ Edad python/demanar.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+numero1=input("Introduce un numero ")
+numero2=input("Introduce un numero ")
+if numero1 == numero2:
+    print "Son numeros iguales"
+else:
+	eleccion=input(Introduzca el ordewn deseado)
+    if num1 > num2:
+        print "El", num1, "es mayor que", num2, ".":
+    else:
+        if num1 < num2:
+            print "El", num2, "
+    
