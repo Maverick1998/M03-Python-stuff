b3a9aa09c7a4cadc6f888490cddb578884993ea1
Maverick1998 <rlrebazamor.elluch@gmail.com>
2017-03-17 12:37:56 +0100

    Add files via upload

---
+++ PYTHON code/ejercicio-swap.py
@@ -0,0 +1,12 @@
+# coding: utf8 
+#Pseudocodigo pasado a python
+#Mis disculpas por no subirlo  a tiempo
+
+mano_der = ("movil")
+mano_izq = ("bocadillo")
+mano_extra = ("movil")
+
+mano_extra = mano_der
+mano_der = mano_izq
+mano_izq = mano_extra
+
