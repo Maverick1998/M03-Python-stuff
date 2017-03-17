80b126450e617a4355c562d98c12118c43c1c2ea
Maverick1998 <rlrebazamor.elluch@gmail.com>
2017-03-17 12:21:57 +0100

    Add files via upload

---
+++ PYTHON code/pepa.py
@@ -0,0 +1,18 @@
+#coding: utf8
+print "Qué desea hacer el amo?"
+print "1.­Sumar"
+print "2.­Restar"
+print "3.­Multiplicar"
+print "4.­Dividir"
+print "5.­Salir"
+opcion = input("")
+
+   if not ( opcion >= 1 and opcion <= 5 ) :
+	   print "Esto es un bucle infinito"
+print "Que quieres hacer?"
+print "1.­Sumar"
+print "2.­Restar"
+print "3.­Multiplicar"
+print "4.­Dividir"
+print "5.­Salir"
+opcion = input("Introduzca la opción deseada:")
