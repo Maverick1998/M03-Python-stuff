dc7abaa1295bfb69a63de6a2937a5e7a33f1ba5d
Maverick1998 <rlrebazamor.elluch@gmail.com>
2017-03-21 09:51:37 +0100

    Add files via upload

---
+++ PYTHON code/plantilla_bucle.py
@@ -0,0 +1,12 @@
+#coding: utf8
+#Robert Rebaza (20-03-17)
+
+
+while (salir == False):
+    print "Hola"
+tecla = raw_input
+if (tecla == "S" or tecla == "S"):
+    print "Adios"
+else:
+    if salir = True
+
