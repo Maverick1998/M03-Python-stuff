67295e6b76100980c054ab138bb3bb24402a9a8d
Maverick1998 <rlrebazamor.elluch@gmail.com>
2017-03-21 11:33:01 +0100

    Add files via upload

---
+++ PYTHON code/Bucle_años.py
@@ -0,0 +1,14 @@
+#coding: utf8
+import math
+
+anyo=2010
+
+sortir=False
+
+while sortir==False:
+    print ("Informe"), anyo
+    anyo = anyo + 1
+    if (anyo > 2016):		
+	    sortir = True
+	    	
+		
